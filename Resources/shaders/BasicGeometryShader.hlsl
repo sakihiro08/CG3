0386d075373492c6121df6a9f46d3043e5b9ee2e
YuukiSakihiro <k021g1259@g.neec.ac.jp>
2022-10-31 10:25:25 +0900

    2-2陥落

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,6 +1,6 @@
 #include"BasicShaderHeader.hlsli"
 
-[maxvertexcount(3)]
+[maxvertexcount(6)]
 void main(
 	triangle VSOutput input[3] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
@@ -12,6 +12,15 @@ void main(
 		element.pos = input[i].svpos;
 		element.normal = input[i].normal;
 		element.uv = input[i].uv;
+		output.Append(element);
+	}
+	output.RestartStrip();
+	for (uint i = 0; i < 3; i++)
+	{
+		GSOutput element;
+		element.pos = input[i].svpos+float4(20.0f,0.0f,0.0f,0.0f);
+		element.normal = input[i].normal;
+		element.uv = input[i].uv * 5.0f;
 		output.Append(element);
 	}
 }
